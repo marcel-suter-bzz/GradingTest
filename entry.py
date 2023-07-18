ac51b5ad2e7f6e39303d7c6fdff777aa0e01d7b9
Marcel Suter <marcel.suter@bzz.ch>
2023-07-18 12:30:58 +0200

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,8 +1,23 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
 class Entry:
+    """
+    an accounting entry for the charging
+    """
+    start_time: str
+    end_time: str
+    release_time: str
+    energy_amount: float
 
+    @property
     def cost(self):
-        return 16.8
-
-
-if __name__ == '__main__':
-    pass
+        """
+        gets the total cost for the charging
+        :return:
+        """
+        energy_cost = self.energy_amount * 0.35
+        return energy_cost
+    
